15d051aee66744f275c7d10d7e954decfd405d9e
Naoya Ito <i.naoya@gmail.com>
2013-06-18 13:27:08 +0900

    Add base-packages cookbook

---
+++ chef-repo/site-cookbooks/base-packages/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: base-packages
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+%w{git}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
